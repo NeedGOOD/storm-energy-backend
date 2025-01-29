cb93168b91da77147f1594ec5f5459e279958076
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-01-29 23:10:45 +0200

    feat: created and added post for creating user

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'users' })
+export class Users {
+  constructor(item: Partial<Users>) {
+    Object.assign(this, item)
+  }
+
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column({ type: 'varchar', length: 255, name: 'first_name', nullable: false })
+  first_name: string
+
+  @Column({ type: 'varchar', length: 255, name: 'last_name', nullable: false })
+  last_name: string
+
+  @Column({ type: 'varchar', length: 255, name: 'email', nullable: false, unique: true })
+  email: string
+
+  @Column({ type: 'varchar', length: 255, name: 'password', nullable: false })
+  password: string
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { EntityManager } from 'typeorm';
+import { Users } from './entities/user.entity';
+// import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly usersRepository: EntityManager
+  ) { }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = new Users(createUserDto)
+    await this.usersRepository.save(user)
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
