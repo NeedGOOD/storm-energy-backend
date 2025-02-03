e7dfbe7da4729a0eee5f5c424f8debd3c57b1d42
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-02-03 16:30:35 +0200

    feat: a request for id user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -5,7 +5,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
@@ -18,7 +18,7 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
