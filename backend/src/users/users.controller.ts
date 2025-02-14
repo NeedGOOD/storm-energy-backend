53d06175e9cbc9466dafc8a2cfe4cb597862b844
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-02-14 13:52:34 +0200

    refactor: removed async

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -9,32 +9,32 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
 
   @Get()
-  async findAll() {
+  findAll() {
     return this.usersService.findAll();
   }
 
   @Get('filter')
-  async findUserByFilter(@Query() filters: FilterUserDto) {
+  findUserByFilter(@Query() filters: FilterUserDto) {
     return this.usersService.findUsersByFilter(filters)
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    await this.usersService.remove(+id);
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
