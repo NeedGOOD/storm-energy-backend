5e71548fa101620957a31dba36b4962fa3fb7cca
Vlad Yakusevych <vladyakusevich@gmail.com>
2025-03-06 20:20:37 +0200

    feat: feat: validate DTO values

--- backend/src/system/dto/update-system.dto.ts
+++ backend/src/system/dto/update-system.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSystemDto } from './create-system.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateSystemDto extends PartialType(CreateSystemDto) {
+  @IsString()
+  @IsNotEmpty()
   location?: string;
 }
